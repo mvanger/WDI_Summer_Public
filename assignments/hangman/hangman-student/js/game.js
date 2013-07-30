790ab9db926d2c57e5487251a7daf5d0d4429dad
mvanger <mvanger@nyu.edu>
2013-07-30 10:26:42 -0400

    s

--- assignments/hangman/hangman-student/js/game.js
+++ assignments/hangman/hangman-student/js/game.js
@@ -1,9 +1,13 @@
 var word = {
+  // This is an attribute of the object word.
   secretWord: "",
   wordList: ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database'],
 
   // Selects a random word from the word list sets the secret word
-  setSecretWord: function(){},
+  setSecretWord: function(){
+    var index = _.random(0, this.wordList.length);
+    this.secretWord = this.wordList[index];
+  },
 
   // Takes an array of letters as input and returns an array of two items:
   // 1) A string with the parts of the secret word that have been guessed correctly and underscore for the parts that haven't
@@ -37,6 +41,7 @@ var game = {
 };
 
 window.onload = function(){
+  console.log(word)
   // Start a new game
   // Add event listener to the letter input field to grab letters that are guessed
   // Add event listener to the reset button to reset the game when clicked
